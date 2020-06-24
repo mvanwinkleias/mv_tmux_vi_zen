7610db0bad33e012444958efce39517059a7e8f9
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-06-24 08:43:46 -0400

    docs

--- src/bin/paneful-tmux.sh
+++ src/bin/paneful-tmux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# One long, thin pane at the bottom (for builds)
+# A tall pane at the left (for editing)
+# 3 panes on the right for agility
+
 tmux new-session \; \
 	split-window -v -p 10 \; \
 	select-pane -t 0 \; \
