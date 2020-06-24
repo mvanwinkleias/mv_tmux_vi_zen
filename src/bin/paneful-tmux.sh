a981f663cc0898abe9062fc5ea1dbd737ce2d04c
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-06-24 09:17:41 -0400

    ready for a test-drive

--- src/bin/paneful-tmux.sh
+++ src/bin/paneful-tmux.sh
@@ -4,10 +4,14 @@
 # A tall pane at the left (for editing)
 # 3 panes on the right for agility
 
+# Scratch:
+# set pane-border-status top \; \
+# select-pane -T 'Editor' \; \
+
 tmux new-session \; \
 	split-window -v -p 10 \; \
 	select-pane -t 0 \; \
 	split-window -h -p 50 \; \
 	split-window -v -p 66 \; \
 	split-window -v -p 50 \; \
-
+	select-pane -t 0 \; \
