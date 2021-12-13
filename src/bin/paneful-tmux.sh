184ee4eab26d60bec61c24810e457bd478aa14cd
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-12-12 19:20:12 -0500

    better session naming, removed vim explore for now

--- src/bin/paneful-tmux.sh
+++ src/bin/paneful-tmux.sh
@@ -9,7 +9,7 @@
 # select-pane -T 'Editor' \; \
 
 session_name=${PWD##*/}
-
+session_name="${session_name}-"$(date '+%Y_%m_%d_%H_%M_%S')
 tmux new-session \; \
 	rename-session "$session_name" \; \
 	split-window -v -p 10 \; \
@@ -18,4 +18,4 @@ tmux new-session \; \
 	split-window -v -p 66 \; \
 	split-window -v -p 50 \; \
 	select-pane -t 0 \; \
-	send-keys "vim -c 'Texplore'" C-m
+#	send-keys "vim -c 'Texplore'" C-m
